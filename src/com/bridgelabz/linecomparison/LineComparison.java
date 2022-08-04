caa2a6c4c422b060c7ca57196c4e59ab7f599757
hp-pc <navyashree811@gmail.com>
2022-08-04 21:27:16 +0530

    [Navaya]Added: Check equality of two lines based on the end points on the end points, so that I know when two lines are the equal

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -1,7 +1,6 @@
 /*
- * As a fan of geometry, I want to model a line based on a point 
- * consisting of (x,y) co-ordinates using the Cartesian system, so that I can 
- * Calculate its length.
+ * As a fan of geometry, I want to check equality of two lines
+ * based on the end points,so that I know when two lines are the equal.
  */
 package com.bridgelabz.linecomparison;
 
@@ -20,11 +19,28 @@ public class LineComparison {
 		int x2 = scanner.nextInt();
 		System.out.println("Enter y2 of Second Point: ");
 		int y2 = scanner.nextInt();
+
+		System.out.println("Enter x1 of Third Point: ");
+		int p1 = scanner.nextInt();
+		System.out.println("Enter y1 of Third Point: ");
+		int q1 = scanner.nextInt();
+		System.out.println("Enter x2 of Forth Point: ");
+		int p2 = scanner.nextInt();
+		System.out.println("Enter y2 of Forth Point: ");
+		int q2 = scanner.nextInt();
 		scanner.close();
 
-		double lenghtOfLine = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.printf("Length of Line is : %.2f %n", lenghtOfLine);
+		Double lenghtOfLine1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		Double lenghtOfLine2 = Math.sqrt(Math.pow(p2 - p1, 2) + Math.pow(q2 - q1, 2));
 
+		System.out.printf("Length of First Line is : %.2f %n", lenghtOfLine1);
+		System.out.printf("Length of Second Line is : %.2f %n", lenghtOfLine2);
+
+		boolean ans = lenghtOfLine1.equals(lenghtOfLine2);
+		if (ans) {
+			System.out.println("Both Lines are Equal");
+		} else {
+			System.out.println("Both Lines are not Equal ");
+		}
 	}
-
 }
