084d2012933deb6638eb26eb3a4cba6b29201ddd
hp-pc <navyashree811@gmail.com>
2022-08-04 21:05:28 +0530

    [Navaya]Added : Calculate length of line

---
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -0,0 +1,30 @@
+/*
+ * As a fan of geometry, I want to model a lien based on a point 
+ * consisting of (x,y) co-ordinates using the Cartesian system, so that I can 
+ * Calculate its length.
+ */
+package com.bridgelabz.linecomparison;
+
+import java.util.Scanner;
+
+public class LineComparison {
+	public static void main(String[] args) {
+		System.out.println("Welcome to The Line Comparison Computation Program");
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter x1 of First Point: ");
+		int x1 = scanner.nextInt();
+		System.out.println("Enter y1 of First Point: ");
+		int y1 = scanner.nextInt();
+		System.out.println("Enter x2 of Second Point: ");
+		int x2 = scanner.nextInt();
+		System.out.println("Enter y2 of Second Point: ");
+		int y2 = scanner.nextInt();
+		scanner.close();
+
+		double lenghtOfLine = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.printf("Length of Line is : %.2f %n", lenghtOfLine);
+
+	}
+
+}
--- src/com/bridgelabz/linecomparison/WelcomeLineComparison_Program.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.linecomparison;
-
-public class WelcomeLineComparison_Program {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Programs");
-	}
-
-}
